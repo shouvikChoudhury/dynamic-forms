deaa0e3e6cd2bf7bdeccd249214f7348576fd1c7
Shouvik Choudhury <98959824+shouvikChoudhury@users.noreply.github.com>
2023-02-28 12:53:46 +0530

    Add files via upload

--- src/components/elements/Input.js
+++ src/components/elements/Input.js
@@ -6,13 +6,12 @@ const Input = ({ field_id, field_label, field_placeholder, field_value }) => {
     return (
         <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">{field_label}</label>
-            <input required type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder={field_placeholder ? field_placeholder : ''}
                 value={field_value}
                 onChange={event => handleChange(field_id, event)}
             />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-
         </div>
     )
 }
